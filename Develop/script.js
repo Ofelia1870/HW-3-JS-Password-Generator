9eeb0ba908351a530da463573e8b808805f813df
Ofelia Sanchez <ofeliasanchez30@gmail.com>
2022-03-12 23:24:41 -0800

    alert boxes

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// NEW CODE ADDED
+var characterAmount = window.prompt ("How many characters would you like included in your password?", 8);
+console.log(characterAmount);
+
+if (confirm("Click OK to include lowercase letters."));
+
+if (confirm("Click OK to include uppercase letters."));
+
+if (confirm("Click OK to include numerical values."));
+
+if (confirm("Click OK to include special characters."));
+
+
+
